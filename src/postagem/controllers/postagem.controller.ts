44f0a4c8542e191fdba523436818005581ed51eb
LuanaTechVet <luanaap2702@gmail.com>
2025-06-24 16:03:29 -0300

    Módulo Postagem com CRUD concluído

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Postagem } from '../entities/postagem.entity';
 import { PostagemService } from '../services/postagem.service';
 
@@ -10,5 +21,40 @@ export class PostagemController {
   @HttpCode(HttpStatus.OK) //códigos de sucesso
   findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
+  }
+
+  @Get('/:id') //mapea as requisições HTTP GET enviadas para o endpoint /postagens/:id. O :id no endpoint é um parâmetro de rota, que representa o identificador da postagem a ser recuperada.
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    //@Param('id') para capturar o valor do parâmetro id enviado na URL da requisição e o insere como argumento no método findById()
+    return this.postagemService.findById(id); //A Classe ParseIntPipe é utilizada para garantir que o valor de id seja tratado como um número inteiro
+  }
+
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findByAllTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+    //novo método, findAllByTitulo foi criado na service
+    //Por questões de boas práticas e legibilidade do código, a Variável de Caminho e o Parâmetro do Método devem possuir o mesmo nome
+    return this.postagemService.findAllByTitulo(titulo);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED) //qualquer requisição POST enviada para esse endereço será tratada pelo Método create da classe controladora
+  //o método é utilizado para criar novos registros no banco de dados, recebendo os dados no corpo da requisição
+  create(@Body() postagem: Postagem): Promise<Postagem> {
+    //Este decorador(@Body) acessa o Objeto postagem enviado no corpo da Requisição e insere no Objeto postagem (parâmetro) do Método create(@Body() postagem: Postagem)
+    return this.postagemService.create(postagem);
+  }
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.update(postagem);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT) //quando a Resposta da Requisição for positiva (o Objeto foi apagado no Banco de dados), será retornado o HTTP Status NO_CONTENT 🡪 204
+  delete(@Param('id', ParseIntPipe) id: number) {
+    //@Param('id'): Este decorator insere o valor enviado na variável de caminho id (:id), no parâmetro do Método delete(@Param('id', ParseIntPipe) id: number);
+    return this.postagemService.delete(id);
   }
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Postagem } from './../entities/postagem.entity';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Postagem } from '../entities/postagem.entity';
+import { DeleteResult, ILike, Repository } from 'typeorm';
 
 @Injectable()
 export class PostagemService {
@@ -12,5 +12,46 @@ export class PostagemService {
 
   async findAll(): Promise<Postagem[]> {
     return await this.postagemRepository.find();
+  }
+
+  async findById(id: number): Promise<Postagem> {
+    const postagem = await this.postagemRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!postagem)
+      throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+
+    return postagem;
+  }
+
+  async findAllByTitulo(titulo: string): Promise<Postagem[]> {
+    return await this.postagemRepository.find({
+      //método find() da Classe postagemRepository (TypeOrm) é chamado para executar a consulta no banco de dados
+      where: {
+        titulo: ILike(`%${titulo}%`), //o ILike não diferencia letras maiúsculas de minúsculas(Like sim) Ele localiza todas as postagens cujo atributo titulo contenha a string enviada no parâmetro
+      }, //${} pq titulo é uma variável que recebe o texto
+    });
+  }
+
+  async create(postagem: Postagem): Promise<Postagem> {
+    //postagem do tipo Postagem (tabela entidade), o método é responsável por receber um objeto da classe Postagem e persistir seus dados no banco de dados
+    return await this.postagemRepository.save(postagem);
+    //apenas os Atributos titulo e texto serão enviados, porquê o id será atribuído pelo Banco de dados após o Objeto ser persistido e a data será atribuída pelo Nest (timestamp).
+  }
+
+  async update(postagem: Postagem): Promise<Postagem> {
+    await this.findById(postagem.id); //O método findById(id: number) da classe PostagemService é chamado para verificar se a postagem que será atualizada existe
+
+    return await this.postagemRepository.save(postagem); //O método save() da classe Repository pode ser usado tanto para persistir um novo objeto quanto para atualizar um objeto existente no banco de dados
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    // O id será enviado pelo Método da Classe PostagemController
+    await this.findById(id);
+
+    return await this.postagemRepository.delete(id);
   }
 }
